f081d262361fb77b27f864e4813194f64f436e7d
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-27 06:26:07 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
 
     self.artists.each do |n|
-      @@all < n.name
+      @@all < n.name:
     end
     @@all
     # return an array of strings containing every musician's name
